8216d5188012472f7ce3841563cf65540d94d575
Ahmet Hakan Öztürk <72892523+PACMAN212@users.noreply.github.com>
2022-11-02 14:17:43 +0300

    Update inverted.py

--- invert/inverted.py
+++ invert/inverted.py
@@ -1,9 +1,13 @@
+#Ahmet Hakan ÖZTÜRK 02205076034
 import cv2
 import numpy as np
+#Orijinal halini göstermek------------------------------
 original_resim = cv2.imread("elmalar.jpg")
 cv2.imshow("Orjinal Resim",original_resim)
+#Gri halini göstermek-----------------------------------
 gri_resim = cv2.imread('elmalar.jpg',0)
 cv2.imshow("Gri Format",gri_resim)
+#Döngü ile görselin invert'ini almak-------------------
 [h,w] = gri_resim.shape
 invert_resim = np.zeros([h,w], dtype=np.uint8)
 for i in range(h):
